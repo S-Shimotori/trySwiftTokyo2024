cf9608030461a85d691a026da502af1488949546
S-Shimotori <gingerale.jagajaga@gmail.com>
2024-02-03 13:52:45 +0900

    Add iOS application project

---
+++ deadman/DeadmanApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeadmanApp
+//
+//  Created by S-Shimotori on 2024/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ deadman/DeadmanApp/DeadmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadmanApp.swift
+//  DeadmanApp
+//
+//  Created by S-Shimotori on 2024/02/03.
+//
+
+import SwiftUI
+
+@main
+struct DeadmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
