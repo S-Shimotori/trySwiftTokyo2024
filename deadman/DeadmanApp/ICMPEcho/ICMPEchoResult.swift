acd3b69dc56af0baaf4509876d9dbf8b03c4abce
S-Shimotori <gingerale.jagajaga@gmail.com>
2024-03-08 21:04:02 +0900

    Add parameters of detail to ICMP Echo result

--- deadman/DeadmanApp/ICMPEcho/ICMPEchoResult.swift
+++ deadman/DeadmanApp/ICMPEcho/ICMPEchoResult.swift
@@ -5,8 +5,21 @@
 //  Created by S-Shimotori on 2024/02/03.
 //
 
+import Foundation
+
 enum ICMPEchoResult {
-    case reply
-    case timeout
-    case error
+    /// - Parameters:
+    ///   - sourceAddress: Who sent this reply.
+    ///   - identifier: The identifier of this reply.
+    ///   - sequenceNumber: The sequence number of this reply.
+    ///   - rtt: Round Trip Time until this reply was received.
+    ///   - data: Payload data.
+    case reply(sourceAddress: in_addr, identifier: UInt16, sequenceNumber: UInt16, rtt: TimeInterval, data: Data?)
+
+    /// - Parameters:
+    ///   - identifier: The identifier of the request.
+    ///   - sequenceNumber: The sequence number of the request.
+    case timeout(identifier: UInt16, sequenceNumber: UInt16)
+
+    case error(any Error)
 }
